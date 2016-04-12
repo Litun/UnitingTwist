b6ecf707ea064ddbca6e4ba010856f33097aec0a
Litun <litunone@gmail.com>
2016-04-13 02:43:28 +0300

    New icon and screen scale

--- app/src/main/java/ru/litun/unitingtwist/MyGLRenderer.java
+++ app/src/main/java/ru/litun/unitingtwist/MyGLRenderer.java
@@ -48,7 +48,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
         // Set the camera position (View matrix)
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 3, 0f, 0f, 0f, /*upX*/ upX, /*upY*/ upY, 0.0f);
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 4f, 0f, 0f, 0f, /*upX*/ upX, /*upY*/ upY, 0.0f);
 
         // Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
